71b6e762c73e4cfa01bff2a7710641b59facfd27
Jann Louie Almirante <jannlouiealmirante@gmail.com>
2023-04-01 21:55:43 +0800

    Create interface in main

--- main.cpp
+++ main.cpp
@@ -3,25 +3,82 @@
 
 using namespace std;
 
+char getAction();
+void createFolder(FileSystem<string> *);
+
 int main() {
-    FileSystem<int> *tree = new FileSystem<int>();
+    FileSystem<string> *systemDirectory = new FileSystem<string>();
 
-    tree->add(-1, "dir", "usr/", "/");
-    tree->add(-1, "dir", "home/", "/usr/");
-    tree->add(-1, "dir", "bin/", "/usr/");
-    tree->add(1, "file", "Name.txt", "/usr/home/");
-    tree->add(2, "file", "Works.txt", "/usr/home/");
-    tree->add(3, "file", "settings.json", "/usr/bin/");
-    tree->add(-1, "dir", "static/", "/usr/bin/");
-    tree->add(4, "file", "static.json", "/usr/bin/static/");
-    tree->add(-1, "dir", "img/", "/usr/bin/static/");
-    tree->add(5, "file", "photo.png", "/usr/bin/static/img/");
-    tree->add(-1, "dir", "favourites/", "/usr/bin/static/img/");
-    tree->add(6, "file", "me.png", "/usr/bin/static/img/favourites/");
-    tree->add(-1, "dir", "system/", "/usr/");
-    tree->add(7, "file", "system.deb", "/usr/system/");
+    char action;
+    bool running = true;
 
-    tree->print(1);
+    while (running) {
+        cout << "<------- FILE SYSTEM SIMULATOR ------->\n";
+        action = getAction();
+
+        system("cls");
+
+        switch (action) {
+            case '1':
+                systemDirectory->print(1);
+                break;
+
+            case '2':
+                systemDirectory->print(0);
+                break;
+
+            case '3':
+                createFolder(systemDirectory);
+                break;
+
+            case '8':
+                delete[] systemDirectory;
+                systemDirectory = new FileSystem<string>();
+                break;
+            
+            case '9':
+                running = false;
+                break;
+        }
+
+        cout << endl;
+    }
+     
 
     return 0;
+}
+
+char getAction() {
+    char choice;
+
+    cout << "\n[ACTIONS]\n";
+    cout << "[1] Show System Tree\n[2] Show System Tree-CMD mode\n"
+        << "[3] Add Folder\n[4] Add File\n"
+        << "[5] Search File/Folder\n[6] Delete File/Folder\n"
+        << "[7] Edit File/Folder name\n[8] Delete all Files and Folders\n" 
+        << "[9] Exit\n"
+        << "Choose action: ";
+    cin >> choice;
+
+    return choice;
+}
+
+void createFolder(FileSystem<string> *systemDirectory) {
+    const string VALUE = "";
+    string folderName;
+    string path;
+
+    cout << "\nEnter folder name: ";
+    getline(cin >> ws, folderName); 
+    folderName += "/";
+
+    cout << "Enter path to put folder into: ";
+    getline(cin >> ws, path);
+
+    if (systemDirectory->verifyPath(path + folderName)) {   
+        cout << "\n[INFO] Folder exists! Aborting...\n";
+        return;
+    }
+
+    systemDirectory->add(VALUE, "dir", folderName, path);
 }
